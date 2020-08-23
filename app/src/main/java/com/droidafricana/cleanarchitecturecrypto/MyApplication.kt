facfc99f0d93abecf28e43c233972010785a72bb
Fbada006 <ferdinandkamata@yahoo.com>
2020-08-23 09:40:20 +0300

    add initial dagger hilt initialization

---
+++ app/src/main/java/com/droidafricana/cleanarchitecturecrypto/MyApplication.kt
@@ -0,0 +1,7 @@
+package com.droidafricana.cleanarchitecturecrypto
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application()
