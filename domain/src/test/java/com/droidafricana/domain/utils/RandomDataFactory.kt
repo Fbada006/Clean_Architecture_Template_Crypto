49ceb4e71d29bde88283d5dd9ec003a177fb3ec3
Fbada006 <ferdinandkamata@yahoo.com>
2020-08-24 17:56:06 +0300

    refactor cryptoTest

--- domain/src/test/java/com/droidafricana/domain/utils/RandomDataFactory.kt
+++ domain/src/test/java/com/droidafricana/domain/utils/RandomDataFactory.kt
@@ -3,6 +3,6 @@ package com.droidafricana.domain.utils
 import com.droidafricana.domain.models.Crypto
 import java.util.UUID.randomUUID
 
-fun randomString() = randomUUID().toString()
+internal fun randomString() = randomUUID().toString()
 
-fun randomCrypto() = Crypto(randomString(), randomString(), randomString())
+internal fun randomCrypto() = Crypto(randomString(), randomString(), randomString())
