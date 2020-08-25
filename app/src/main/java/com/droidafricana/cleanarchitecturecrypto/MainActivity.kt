75e90ea78f37ea34c8531e88febc97fde0217a71
Fbada006 <ferdinandkamata@yahoo.com>
2020-08-25 18:42:03 +0300

    add code documentation

--- app/src/main/java/com/droidafricana/cleanarchitecturecrypto/MainActivity.kt
+++ app/src/main/java/com/droidafricana/cleanarchitecturecrypto/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 
+/**At the moment, this activity only shows the crypto fragment.
+ * Modify this as you wish in line with the navigation component's functionality*/
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
