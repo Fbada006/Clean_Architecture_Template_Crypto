1b8111f32d6bf5ed92e63d111168880043c59be1
Fbada006 <ferdinandkamata@yahoo.com>
2020-08-25 18:45:55 +0300

    use constants instead of magic numbers

--- data/src/main/java/com/droidafricana/data/api/HttpClient.kt
+++ data/src/main/java/com/droidafricana/data/api/HttpClient.kt
@@ -1,5 +1,7 @@
 package com.droidafricana.data.api
 
+import com.droidafricana.data.utils.API_CONNECT_TIMEOUT
+import com.droidafricana.data.utils.API_READ_TIMEOUT
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import java.util.concurrent.TimeUnit
@@ -9,8 +11,8 @@ object HttpClient {
     fun create(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
         return OkHttpClient.Builder()
             .addInterceptor(httpLoggingInterceptor)
-            .connectTimeout(5, TimeUnit.SECONDS)
-            .readTimeout(5, TimeUnit.SECONDS)
+            .connectTimeout(API_CONNECT_TIMEOUT, TimeUnit.SECONDS)
+            .readTimeout(API_READ_TIMEOUT, TimeUnit.SECONDS)
             .build()
     }
 }
---
+++ data/src/main/java/com/droidafricana/data/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.droidafricana.data.utils
+
+const val API_READ_TIMEOUT = 5L
+
+const val API_CONNECT_TIMEOUT = 5L
