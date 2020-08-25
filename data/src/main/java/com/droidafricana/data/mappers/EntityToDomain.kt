44faeb3be4d91fbe845214ae927774cd84f97fac
Fbada006 <ferdinandkamata@yahoo.com>
2020-08-25 18:43:41 +0300

    remove TODOs

--- data/src/main/java/com/droidafricana/data/mappers/EntityToDomain.kt
+++ data/src/main/java/com/droidafricana/data/mappers/EntityToDomain.kt
@@ -3,7 +3,6 @@ package com.droidafricana.data.mappers
 import com.droidafricana.data.models.CryptoEntity
 import com.droidafricana.domain.models.Crypto
 
-// TODO Map the whole list
 internal fun CryptoEntity.toDomain() = Crypto(
     this.id, this.name, this.symbol
 )
